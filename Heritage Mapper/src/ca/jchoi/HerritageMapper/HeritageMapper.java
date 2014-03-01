cc020603b9f4fd25631a5bd15e71c7702836c44f
aihchen <angelaichen87@gmail.com>
2014-03-01 01:53:28 -0800

    Added CSVReader Added CSVWriter Added HeritageMapper.app Added Heritage CSV template to assets folder Modified PointOfInterest Modified AndroidManifest.xml

---
+++ Heritage Mapper/src/ca/jchoi/HerritageMapper/HeritageMapper.java
@@ -0,0 +1,58 @@
+package ca.jchoi.HerritageMapper;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Application;
+import android.util.Log;
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
+
+public class HeritageMapper extends Application {
+	
+	private static final String TAG = "HeritageMapper";
+	private List<PointOfInterest> masterList = new ArrayList<PointOfInterest>();
+
+	// Add method to check if master list is already created
+	
+	@Override
+	public void onCreate() {
+		try { // Reading CSV template from the assets directory and creating a master list of point of interest objects
+			String assetPath = "NHSC_LHNC_TB.csv";
+			InputStream assetStream = getAssets().open(assetPath);
+			InputStreamReader reader = new InputStreamReader(assetStream);
+			CSVReader csvReader = new CSVReader(reader);
+			String columns[];
+			while ((columns = csvReader.readNext()) != null) {
+				PointOfInterest poi = PointOfInterest.create(columns);
+				masterList.add(poi);
+			}
+			// Writing master list of point of interest objects out as a CSV file
+			FileOutputStream output = openFileOutput("masterlist.csv", MODE_APPEND); // may have to revisit mode_append b/c there should be a MODE_REPLACE so Append will have duplicate files
+			OutputStreamWriter writer = new OutputStreamWriter(output);
+			CSVWriter csvWriter = new CSVWriter(writer);
+			for (PointOfInterest poi : masterList) {
+				columns = poi.getColumns();
+				csvWriter.writeNext(columns);
+			}
+			// Read the master list CSV file back in and print it out THIS IS FOR TESTING ONLY!!!
+			FileInputStream input = openFileInput("masterlist.csv");
+			reader = new InputStreamReader(input);
+			csvReader = new CSVReader(reader);
+			while ((columns = csvReader.readNext()) != null) {
+				for (String value : columns) {
+					Log.i(TAG, value);
+				}
+			}
+		} catch (IOException e) {//input & output reading exception
+			e.printStackTrace();
+		}
+		//need to add an accessor in this application class to get the master list so that all the activities can use the master list
+	}
+}
--- Heritage Mapper/src/ca/jchoi/HerritageMapper/PointOfInterest.java
+++ Heritage Mapper/src/ca/jchoi/HerritageMapper/PointOfInterest.java
@@ -2,22 +2,102 @@ package ca.jchoi.HerritageMapper;
 
 public class PointOfInterest {
 
+	private static final int SITE_ID = 0;
+	private static final int NAME = 1;
+	private static final int NAME_FRENCH = 2;
+	private static final int STREET = 3;
+	private static final int PLAQUE_LOCATION = 4;
+	private static final int TOWN = 5;
+	private static final int PROV = 6;
+	private static final int REASON_FOR_DESIGNATION = 7;
+	private static final int REASON_FOR_DESIGNATION_FRENCH = 8;
+	private static final int LATITUDE= 9;
+	private static final int LONGITUDE = 10;
+	private static final int NUM_COLUMNS = 11;
+	
 	int SiteID;
 	String name;
 	String nameFrench;
+	String street;
+	String plaqueLocation;
 	String province;
 	String town;
-	String street;
+	
 	String designation;
 	String designationFrench;
-	String coordinates;
+	float latitude;
+	float longitude;
 	boolean wantToVisit;
 	boolean Visited;
+	
+	public static PointOfInterest create(String[] data) {
+		PointOfInterest poi = new PointOfInterest();
+		poi.setSiteID(getIntegerValue(data[SITE_ID]));
+		poi.setName(getStringValue(data[NAME]));
+		poi.setNameFrench(getStringValue(data[NAME_FRENCH]));
+		poi.setStreet(getStringValue(data[STREET]));
+		poi.setPlaqueLocation(getStringValue(data[PLAQUE_LOCATION]));
+		poi.setTown(getStringValue(data[TOWN]));
+		poi.setProvince(getStringValue(data[PROV]));
+		poi.setDesignation(getStringValue(data[REASON_FOR_DESIGNATION]));
+		poi.setDesignationFrench(getStringValue(data[REASON_FOR_DESIGNATION_FRENCH]));
+		poi.setLatitude(getFloatValue(data[LATITUDE]));
+		poi.setLongitude(getFloatValue(data[LONGITUDE]));
+		return poi;
+	}
 	
 	public PointOfInterest(){
 		
 	}
 	
+	private static String getStringValue(String data) {
+		if (data == null || data.length() == 0) {
+			return "";
+		}
+		return data;
+	}
+	
+	private static float getFloatValue(String data) {
+		try {
+			float value = Float.valueOf(data);
+			return value;
+		} catch (NumberFormatException e) {
+			return 0.0f;
+		}
+	}
+	
+	private static int getIntegerValue(String data) {
+		try {
+			int value = Integer.valueOf(data);
+			return value;
+		} catch (NumberFormatException e) {
+			return 0;
+		}
+	}
+	
+	public String[] getColumns() {
+		String[] columns = new String[NUM_COLUMNS];
+		columns[SITE_ID] = String.valueOf(getSiteID());
+		columns[NAME] = getName();
+		columns[NAME_FRENCH] = getNameFrench();
+		columns[STREET] = getStreet();
+		columns[PLAQUE_LOCATION] = getPlaqueLocation();
+		columns[TOWN] = getTown();
+		columns[PROV] = getProvince();
+		columns[REASON_FOR_DESIGNATION] = getDesignation();
+		columns[REASON_FOR_DESIGNATION_FRENCH] = getDesignationFrench();
+		columns[LATITUDE] = String.valueOf(getLatitude());
+		columns[LONGITUDE] = String.valueOf(getLongitude());
+		return columns;
+	}
+	
+	public String getPlaqueLocation() {
+		return plaqueLocation;
+	}
+	
+	public void setPlaqueLocation(String plaque) {
+		this.plaqueLocation = plaque;
+	}
 
 	public int getSiteID() {
 		return SiteID;
@@ -84,13 +164,25 @@ public class PointOfInterest {
 	}
 
 	public String getCoordinates() {
-		return coordinates;
+		return String.valueOf(latitude) + ", " + String.valueOf(longitude);
 	}
 
-	public void setCoordinates(String coordinates) {
-		this.coordinates = coordinates;
+	public float getLatitude() {
+		return latitude;
+	}
+	
+	public float getLongitude() {
+		return longitude;
+	}
+	
+	public void setLatitude(float latitude) {
+		this.latitude = latitude;
 	}
 
+	public void setLongitude(float longitude) {
+		this.longitude = longitude;
+	}
+	
 	public boolean isWantToVisit() {
 		return wantToVisit;
 	}
