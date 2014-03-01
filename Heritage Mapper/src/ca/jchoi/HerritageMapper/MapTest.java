9a2c8a146494b304802de23ef6ad865184e796a3
Roxanne Taylor <roxanne.e.taylor@gmail.com>
2014-03-01 12:25:43 -0800

    Working on Maps. Doesn't work, but there is sample maps project to creep

--- Heritage Mapper/src/ca/jchoi/HerritageMapper/MapTest.java
+++ Heritage Mapper/src/ca/jchoi/HerritageMapper/MapTest.java
@@ -2,21 +2,20 @@ package ca.jchoi.HerritageMapper;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NavUtils;
 import android.view.Menu;
+import android.view.MenuItem;
 
-public class MapTest extends Activity {
+public class MapTest extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map_test);
 	}
+	
+	
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.map_test, menu);
-		return true;
-	}
-
+	
 }
