8b4e3aed84eb3fa540d681392ba22ab50b04722c
Roxanne Taylor <roxanne.e.taylor@gmail.com>
2014-03-02 13:36:24 -0800

    no message

--- Heritage Mapper/src/ca/jchoi/HerritageMapper/AboutActivity.java
+++ Heritage Mapper/src/ca/jchoi/HerritageMapper/AboutActivity.java
@@ -4,7 +4,8 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class AboutActivity extends Activity {
+public class AboutActivity extends
+android.support.v4.app.FragmentActivity  {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
