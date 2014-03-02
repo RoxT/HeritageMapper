3f836b71940bbb5d63447c9d906c1746a8ca1d39
WoongGun <woonggun@jchoi.ca>
2014-03-02 00:38:26 -0800

    BugFixes 3
    removed more errors

--- Heritage Mapper/src/ca/jchoi/HerritageMapper/VisitedListActivity.java
+++ Heritage Mapper/src/ca/jchoi/HerritageMapper/VisitedListActivity.java
@@ -25,24 +25,14 @@ public class VisitedListActivity extends android.support.v4.app.FragmentActivity
 		getMenuInflater().inflate(R.menu.visited_list, menu);
 		return true;
 	}
-	private void setupActionBar() {
 
-		getActionBar().setDisplayHomeAsUpEnabled(true);
-	}
 	
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
-			NavUtils.navigateUpFromSameTask(this);
+			finish();
 			return true;
 		case R.id.action_search:
         	openSearch();
