d55817588bbba6c1bff683c729bfb579284ffe5a
WoongGun <woonggun@jchoi.ca>
2014-03-02 00:46:38 -0800

    Rollback
    undo errors

--- Heritage Mapper/src/ca/jchoi/HerritageMapper/DisplayMessageActivity.java
+++ Heritage Mapper/src/ca/jchoi/HerritageMapper/DisplayMessageActivity.java
@@ -32,19 +32,21 @@ public class DisplayMessageActivity extends android.support.v4.app.FragmentActiv
 	    setContentView(textView);
 	}
 
-	/**
-	 * Set up the {@link android.app.ActionBar}.
-	 */
 
-	
 
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case android.R.id.home:
-
-			finish();
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
 			return true;
 		case R.id.action_search:
         	openSearch();
